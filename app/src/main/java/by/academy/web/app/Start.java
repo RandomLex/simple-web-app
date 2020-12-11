5f35131bcac5c7a7e01fb68114d17fd4a702245a
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2020-12-11 23:42:16 +0300

    * web-xml -> annotation + to pom.xml plugin configured to work without web-xml

--- app/src/main/java/by/academy/web/app/Start.java
+++ app/src/main/java/by/academy/web/app/Start.java
@@ -1,17 +1,19 @@
 package by.academy.web.app;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(value = "/hello")
 public class Start extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
-        writer.write("<p><span style='color: blue;'>Hello, world!</span></p>");
+        writer.write("<p><span style='color: blue;'>Hello, world, without web-xml</span></p>");
     }
 }
